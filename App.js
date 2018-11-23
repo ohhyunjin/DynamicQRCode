65dd049219095d95cd533d43a336e068cca65ce6
ohhyunjin <javieroh@hotmail.com>
2018-11-23 14:01:26 -0800

    QR code not changing bug fix

--- App.js
+++ App.js
@@ -1,39 +1,32 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import QRCode from 'react-native-qrcode';
-import timer from 'react-native-timer';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-        seconds: 0,
-        number: 1,
+        text: 'scanned',
     };
 
     setInterval(() => {
       this.setState(previousState => (
         {
-           seconds: previousState.seconds + 1,
-           number: Math.floor(Math.random() * 100) + 1,
+           text: previousState.text + (Math.floor(Math.random() * 100) + 1).toString(),
         }
       ))
-  }, 1000);
-    console.log(this.state.seconds);
-    console.log(this.state.number);
+    }, 15000);
   }
 
   render() {
     return (
       <View style={styles.container}>
         <QRCode
-          value={this.state.number}
+          value={this.state.text}
           size={330}
           bgColor='purple'
           fgColor='#F5FCFF'/>
-        <Text>{this.state.seconds}</Text>
-        <Text>{this.state.number}</Text>
       </View>
     );
   }
