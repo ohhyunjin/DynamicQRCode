2a0d7a4c913d6c90a116a926d040476bb553770b
ohhyunjin <javieroh@hotmail.com>
2018-11-22 21:25:59 -0800

    Minor fix

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import QRCode from 'react-native-qrcode';
-import TimerMixin from 'react-timer-mixin';
+import timer from 'react-native-timer';
 
 export default class App extends Component {
   constructor(props) {
@@ -11,37 +11,29 @@ export default class App extends Component {
         seconds: 0,
         number: 1,
     };
+
+    setInterval(() => {
+      this.setState(previousState => (
+        {
+           seconds: previousState.seconds + 1,
+           number: Math.floor(Math.random() * 100) + 1,
+        }
+      ))
+  }, 1000);
     console.log(this.state.seconds);
-  }
-
-  tick() {
-      this.setState(prevState => ({
-          seconds: prevState.seconds + 1
-      }));
-  }
-
-  componentDidMount() {
-      this.interval = setInterval(() => this.tick.bind(this), 2000);
-  }
-
-  componentWillUnmount() {
-      clearInterval(this.interval);
-  }
-
-  generateRandomNumber() {
-      var randomNumber = Math.floor(Math.random() * 100) + 1;
-      return randomNumber;
+    console.log(this.state.number);
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>First dynamic QR code app</Text>
         <QRCode
           value={this.state.number}
           size={330}
           bgColor='purple'
           fgColor='#F5FCFF'/>
+        <Text>{this.state.seconds}</Text>
+        <Text>{this.state.number}</Text>
       </View>
     );
   }
